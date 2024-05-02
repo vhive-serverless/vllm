0f21451a73c1d6e8c934191c1899a356681074c8
lrq619 <lrq619@outlook.com>
2024-05-02 02:12:53 +0000

    reshape no error

--- csrc/cache_kernels.cu
+++ csrc/cache_kernels.cu
@@ -187,22 +187,20 @@ __global__ void reshape_and_cache_kernel(
     const int x_idx = head_offset / x;
     const int x_offset = head_offset % x;
 
-    // const int64_t tgt_key_idx = block_idx * num_heads * (head_size / x) * block_size * x
-    //                             + head_idx * (head_size / x) * block_size * x
-    //                             + x_idx * block_size * x
-    //                             + block_offset * x
-    //                             + x_offset;
-    const int64_t tgt_key_idx = block_idx * num_heads * head_size * block_size
-                                  + head_idx * head_size * block_size
-                                  + head_offset * block_size
-                                  + block_offset;
+    const int64_t tgt_key_idx = block_idx * num_heads * (head_size / x) * block_size * x
+                                + head_idx * (head_size / x) * block_size * x
+                                + x_idx * block_size * x
+                                + block_offset * x
+                                + x_offset;
+    // const int64_t tgt_key_idx = block_idx * num_heads * head_size * block_size
+    //                               + head_idx * head_size * block_size
+    //                               + head_offset * block_size
+    //                               + block_offset;
 
     const int64_t tgt_value_idx = block_idx * num_heads * head_size * block_size
                                   + head_idx * head_size * block_size
                                   + head_offset * block_size
                                   + block_offset;
-    assert(tgt_key_idx < 10240 * num_heads * (head_size /x) * block_size * x);
-    assert(tgt_value_idx < 10240 * num_heads * head_size * block_size);
     scalar_t tgt_key = key[src_key_idx];
     scalar_t tgt_value = value[src_value_idx];
     if constexpr (is_fp8_e5m2_kv_cache) {
@@ -250,8 +248,8 @@ void reshape_and_cache(
   int num_heads = key.size(1);
   int head_size = key.size(2);
   int block_size = key_cache.size(3);
-  // int x = key_cache.size(4);
-  int x = 8;
+  int x = key_cache.size(4);
+  // int x = 8;
 
   int key_stride = key.stride(0);
   int value_stride = value.stride(0);
