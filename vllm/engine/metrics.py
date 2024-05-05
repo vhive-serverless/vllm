dd9fe9225a02f44d36f51ed5169bedc3d5a12cca
shaosy <siyang.shao@outlook.com>
2024-05-05 12:40:19 +0800

    feat: add metrics record (#2)
    * feat: add metrics record
    
    * fix: metrics args

--- vllm/engine/metrics.py
+++ vllm/engine/metrics.py
@@ -115,6 +115,19 @@ class Stats:
     time_per_output_tokens: List[float]
     time_e2e_requests: List[float]
 
+@dataclass
+class EngineMetrics:
+    now: float
+    num_running: int
+    num_waiting: int
+    num_swapped: int
+    gpu_cache_usage: float
+    cpu_cache_usage: float
+    num_prompt_tokens: int
+    num_generation_tokens: int
+    num_output_tokens: int
+    num_total_tokens: int
+
 
 class StatLogger:
     """StatLogger is used LLMEngine to log to Promethus and Stdout."""
