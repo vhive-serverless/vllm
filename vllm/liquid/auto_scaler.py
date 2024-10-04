108b9a2fd2805de53498959516c34836b060e192
lairuiqi <lrq619@outlook.com>
2024-10-04 15:03:59 +0100

    add is_full_flag

--- vllm/liquid/auto_scaler.py
+++ vllm/liquid/auto_scaler.py
@@ -7,7 +7,7 @@ import time
 SCALE_OUT_THRESH_MAP = {
     1: 3,
     2: 3,
-    4: 10,
+    4: 3,
 }
 
 SCALE_IN_THRESH = 0.7
@@ -30,6 +30,8 @@ class AutoScaler:
 
         self.num_gpu_blocks_stack: List[int] = []
 
+        self.is_full_flag: bool = False # The flag is set to True when this instance has reached its liquid gpu range but still wants to scale out, will be set to False again when it is reported to global scaler
+
     def step(self, concurrent_cache_usage,num_using_gpu_blocks, metrics) -> Optional[LiquidRequest]:
         self.cache_usage_records.append(concurrent_cache_usage) 
         self.tp_level_records.append(self.current_tp_level)
@@ -50,11 +52,6 @@ class AutoScaler:
                 scale_out_records_index_window.append(index)
             else:
                 break
-        # for i, ts in enumerate(self.timestamp_records):
-        #     if latest_timestamp - ts < SCALE_OUT_WINDOW:
-        #         scale_out_records_index_window.append(i)
-        #     else:
-        #         break
         # find out all records within scale-in window 
         for i in range(len(self.timestamp_records)):
             index = (len(self.timestamp_records) - 1) - i
@@ -63,10 +60,6 @@ class AutoScaler:
                 scale_in_records_index_window.append(index)
             else:
                 break
-        # scale_in_records_index_window = []
-        # for i, ts in enumerate(self.timestamp_records):
-        #     if latest_timestamp - ts < SCALE_IN_WINDOW:
-        #         scale_in_records_index_window.append(i)
         # If the time window only contains one element, do not scale
         cache_usages = [self.cache_usage_records[i] for i in scale_out_records_index_window]
         cache_usages = np.array(cache_usages)
@@ -93,6 +86,7 @@ class AutoScaler:
         return None
 
     def _scale_in(self) -> Optional[LiquidRequest]:
+        # TODO: when current tp level is 1 and still try to scale in, this should also be reported to global scaler
         if self.current_tp_level == 2:
             self.current_tp_level = 1
             return LiquidRequest(LiquidType.LIQUID_2_1)
@@ -104,6 +98,9 @@ class AutoScaler:
         
 
     def _scale_out(self) -> Optional[LiquidRequest]:
+        if self.current_tp_level >= self.liquid_gpu_num:
+            self.is_full_flag = True
+
         if self.current_tp_level == 1:
             self.current_tp_level = 2
             return LiquidRequest(LiquidType.LIQUID_1_2)
